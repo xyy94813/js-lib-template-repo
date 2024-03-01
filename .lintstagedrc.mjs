319739d9268d04e37eec6dcbb92c4e5aa486e824
RoXoM <xyy94813@sina.com>
2024-03-01 17:10:18 +0800

    refactor: format lint-staged conf file

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,4 @@
-export default { 
-    "*.{md,mdx,xml,html,json,yaml,graphql}": "prettier --write",
-    "*.{ts,js,cjs,mjs,jsx,tsx}": "eslint --fix"
-}
-  
+export default {
+  '*.{md,mdx,xml,html,json,yaml,graphql}': 'prettier --write',
+  '*.{ts,js,cjs,mjs,jsx,tsx}': 'eslint --fix',
+};
